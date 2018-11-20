c2f56a1adba2ebcee32e1598afaf18bc945029a8
Achen725 <anchrenalins@gmail.com>
2018-11-19 23:08:29 -0500

    Sort of working water

--- main.py
+++ main.py
@@ -8,21 +8,40 @@ import random
 import copy
 import math
 
-#totalParticles = []
+def convertToIJK(x, y, z):
+	i = (int((x + 0.5) * 10)) % 2
+	j = (int((y + 0.5) * 10)) % 2
+	k = (int((z + 0.5) * 10)) % 2
+	return int((i * 5 + j) + (z * 25))
+
+def recalulateVelocity(velocities):
+	newVelocities = []
+	for v in velocities:
+		x = sum([pair[0] for pair in v])
+		y = sum([pair[1] for pair in v])
+		z = sum([pair[2] for pair in v])
+
+		if(len(v) == 0):
+			newVelocities.append((0,0,0))
+		else:
+			newVelocities.append((x/len(v), y/len(v), z/len(v)))
+	return newVelocities
 
 class System(object):
 	def __init__(self):
 		self.name = random.random()
 		self.getTicksLastFrame = 0
 		self.movementX = 0.3
-		self.movementZ = 0.1
-		self.totalParticles = self.makeParticles(400)
+		self.movementZ = 0.3
+		self.totalParticles = self.makeParticles(10000)
+		self.velocities = []*125
 
 	def solidCube(self):
 		sides = [(0,1),(0,3),(0,5),(2,1),(2,3),(2,7),
 				(6,1),(6,5),(6,7),(4,5),(4,7),(4,3)]
-		vertex = [(-1,1,1),(1,1,1),(1,1,-1),(-1,1,-1),(-1,-1,-1),
-				(-1,-1,1),(1,-1,1),(1,-1,-1)]
+
+		vertex = [(-0.5,0.5,0.5),(0.5,0.5,0.5),(0.5,0.5,-0.5),(-0.5,0.5,-0.5),(-0.5,-0.5,-0.5),
+				(-0.5,-0.5,0.5),(0.5,-0.5,0.5),(0.5,-0.5,-0.5)]
 
 		glBegin(GL_LINES)
 		glColor3f(1,1,1)
@@ -32,26 +51,42 @@ class System(object):
 		glEnd()
 		glFlush()
 
-	def move(self, x, y, z):
+	def move(self):
+		# take each particle in grid and assign to matrix
+		gridVelocities =  [ [] for i in range(125) ]
 		for particle in self.totalParticles:
-			particle.movePart(x,y,z)
+			index = convertToIJK(particle.x, particle.y, particle.z)
+			gridVelocities[index].append(particle.velocity)
+		# interpolate velocities of each grid
+		newVelocities = recalulateVelocity(gridVelocities)
+
+		# add grid velocity to each particle
+		# avg with total grid velocity
+		for particle in self.totalParticles:
+			index = convertToIJK(particle.x, particle.y, particle.z)
+
+			velocity = (particle.velocity[0] + particle.acceleration[0], particle.velocity[1] + particle.acceleration[1], particle.velocity[2] + particle.acceleration[2])
+
+			velocityPic = (velocity[0] - newVelocities[index][0], velocity[1] - newVelocities[index][1], velocity[2] - newVelocities[index][2])
+
+			x = sum([pair[0] for pair in [velocity,newVelocities[index]]])
+			y = sum([pair[1] for pair in [velocity,newVelocities[index]]])
+			z = sum([pair[2] for pair in [velocity,newVelocities[index]]])
+
+			avg = (x/2,y/2,z/2)
+			particle.updateVelocity(avg[0],avg[1],avg[2])
+			particle.movePart(1,2,3)
 
 	def makeParticles(self,num):
 		lst = []
 		for i in range(num):
-			#randX = round(random.uniform(-0.2, 0.2),2)
-			randX = round(random.uniform(-1, 1),2)
-			randZ = round(random.uniform(-1, 1),2)
-			randY = round(random.uniform(-1.0, -0.5), 2)
-			#randZ = round(random.uniform(-0.2, 0.2),2)
+			randX = round(random.uniform(-0.2, 0.2),2)
+			#randX = round(random.uniform(-1, 1),2)
+			#randZ = round(random.uniform(-1, 1),2)
+			randY = round(random.uniform(0, 0.5), 2)
+			randZ = round(random.uniform(-0.2, 0.2),2)
 			lst.append(Particle(randX, randY, randZ))
-			#totalParticles[-1].drawParticle()
 		return lst
-
-flowRate = 4
-elasticity = 0.4
-running = True
-filled = False
 
 class Particle(object):
 
@@ -60,78 +95,67 @@ class Particle(object):
 		self.y = y
 		self.z = z
 		self.prop = (self.x,self.y,self.z)
-		self.mass = .5
-		self.size = 3
-		self.gravity = -0.02
 		initialSpeed = 0.4
-		self.speed = initialSpeed * random.random() / self.size
-		angle = 360 * random.random()
-		self.dx = self.speed * math.cos(angle)
-		self.dz = self.speed * math.sin(angle)
-
-	def __eq__(self,other):
-		if self.getHashables() == other.getHashables():
-			return True
-		return False
+		self.speed = 0.0002
+		self.velocity = (0, 0, 0)
+		self.acceleration = (0, -9.8, 0)
 
 	def __repr__(self):
 		return "Particle (" + str(self.x) + "," + str(self.y) + str(self.z) + ")"
 
-	def getHashables(self):
-		return (self.prop, self.mass,self.size, self.dx, self.dz, self.speed)
-
 	def movePart(self,dx, dy, dz):
-		xBound = -1 <= self.x + dx <= 1
-		yBound = -1 <= self.y + dy <= 1
-		zBound = -1 <= self.z + dz <= 1
+		dx = (self.velocity[0] * self.speed)
+		dy = (self.velocity[1] * self.speed)
+		dz = (self.velocity[2] * self.speed)
+
+		xBound = -0.5 <= self.x + dx <= 0.5
+		yBound = -0.5 <= self.y + dy <= 0.5
+		zBound = -0.5 <= self.z + dz <= 0.5
+
 		if not xBound or not yBound or not zBound:
-			pass
+			velocity = self.velocity
+			if(self.x + dx < -0.5):
+				self.velocity = (random.uniform(-100, 100),velocity[1], velocity[2])
+			elif(self.x + dx > 0.5):
+				self.velocity = (random.uniform(-100, 100),velocity[1], velocity[2])
+			if(self.y + dy < -0.5):
+				self.velocity = (velocity[0]+random.uniform(-500, 500),random.uniform(0, 500), velocity[2]+random.uniform(-500, 500))
+			elif(self.y + dy > 0.5):
+				self.velocity = (velocity[0]+random.uniform(-500, 500),random.uniform(-500, 0), velocity[2]+random.uniform(-500, 500))
+			if(self.z + dz < -0.5):
+				self.velocity = (velocity[0], velocity[1],random.uniform(-100, 100))
+			elif(self.z + dz > 0.5):
+				self.velocity = (velocity[0], velocity[1],random.uniform(-100, 100))
 		else:
-			self.x += dx
-			self.y += dy
-			self.z += dz
+			velocity = self.velocity
+			self.x += (velocity[0] * self.speed)
+			self.y += (velocity[1] * self.speed)
+			self.z += (velocity[2] * self.speed)
+
+	def updateVelocity(self, dx, dy, dz):
+		self.velocity = (dx, dy, dz)
 
 	def collides(self, totalParticles):
 		if self in totalParticles:
 			return True
-		if  -1 > self.x > 1:
+		if  -0.5 > self.x > 0.5:
 			return True
-		if  -1 > self.y > 1:
+		if  -0.5 > self.y > 0.5:
 			return True
-		if  -1 > self.z > 1:
+		if  -0.5 > self.z > 0.5:
 			return True
 		return False
 
 	def drawParticle(self):
+		glPointSize(5.0)
 		glBegin(GL_POINTS)
 		glVertex3f(self.x, self.y, self.z)
 		glColor3f(0,0.5,1)
+		# glPointSize(2.0)
 		glEnd()
 
 	def fillSpace(self):
 		pass
-
-# def applyBound(xy, vel, xlim, ylim, zlim, damp, i):
-#     if xy[i, 0] < xlim[0]:
-#         vel[i, 0] *= damp
-#         xy[i, 0] = xlim[0]
-#     elif xy[i, 0] > xlim[1]:
-#         vel[i, 0] *= damp
-#         xy[i, 0] = xlim[1]
-#
-#     if xy[i, 1] < ylim[0]:
-#         vel[i, 1] *= damp
-#         xy[i, 1] = ylim[0]
-#     elif xy[i, 1] > ylim[1]:
-#         vel[i, 1] *= damp
-#         xy[i, 1] = ylim[1]
-#
-# def eulerStep(xy, vel, force, rho, dt, xlim, ylim, damp, hashmap):
-#     for i in range(xy.shape[0]):
-#         vel[i] += dt * force[i] / rho[i]
-#         xy[i] += dt * vel[i]
-#         applyBound(xy, vel, xlim, ylim, zlim, damp, i)
-#         hashmap.move(i, xy[i])
 
 def drawParticles(totalParticles):
 	for particle in totalParticles:
@@ -148,22 +172,7 @@ def update(deltaTime, mySystem):
 	totalParticles = mySystem.totalParticles
 	xMove = mySystem.movementX
 	zMove = mySystem.movementZ
-	#if deltaTime % 1 == 0:
-	for particle in totalParticles:
-		x,y,z = particle.prop
-		grav = particle.gravity * particle.mass
-		temp = Particle(x,y+grav, z)
-		if not filled and -1 <= y + grav <= 1 and not temp.collides(totalParticles):
-			particle.movePart(0,grav,0)
-			particle.drawParticle()
-
-	# if deltaTime % 2:
-	# 	move(xMove,0,0)
-	# 	mySystem.movementX = - mySystem.movementX
-
-	# if deltaTime % 4:
-	# 	move(0,0,zMove)
-	# 	mySystem.movementZ = - mySystem.movementZ
+	mySystem.move()
 
 def mainLoop(simulation):
 	pygame.init()
@@ -172,7 +181,7 @@ def mainLoop(simulation):
 	screen = pygame.display.set_mode(display, DOUBLEBUF|OPENGL)
 	# camera
 	gluPerspective(45, (display[0]/display[1]), 0.1, 50.0)
-	glTranslatef(0.0,0.0, -10)
+	glTranslatef(0.0,0.0, -5)
 
 	while True:
 		for event in pygame.event.get():
@@ -188,14 +197,6 @@ def mainLoop(simulation):
 					glRotatef(6,1,0,0)
 				elif event.key == pygame.K_DOWN:
 					glRotatef(6,-1,0,0)
-				elif event.key == pygame.K_w:
-					simulation.move(0,0.2,0)
-				elif event.key == pygame.K_s:
-					simulation.move(0,-0.2,0)
-				elif event.key == pygame.K_a:
-					simulation.move(-0.2,0,0)
-				elif event.key == pygame.K_d:
-					simulation.move(0.2,0,0)
 
 		# timer to keep continuous movement
 		t = pygame.time.get_ticks()
@@ -210,7 +211,7 @@ def mainLoop(simulation):
 		drawParticles(simulation.totalParticles)
 
 		# size manipulation of particles
-		glPointSize(3)
+		glPointSize(20)
 		#update the frame
 		pygame.display.flip()
 
